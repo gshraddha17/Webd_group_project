48c8482f775b80298c2722c8a35581228d147d6f
gshraddha17 <94107629+gshraddha17@users.noreply.github.com>
2022-04-09 17:59:53 +0530

    Add files via upload

---
+++ shop_reviews_api/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import FetchData from './components/FetchData'
+const App = () => {
+    return(
+        <div>
+            <FetchData />
+        </div>
+    )
+}
+export default App
